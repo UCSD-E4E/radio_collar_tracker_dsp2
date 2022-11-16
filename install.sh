12f9b21378521c87a9e137b81087e9682a05b4d0
Nathan Hui <ntlhui@gmail.com>
2022-11-15 21:37:25 -0800

    Added upgrade and yes

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get install -y libboost-all-dev python3-pip python3-mako python3-venv libusb-1.0-0-dev cmake build-essential pkg-config libfftw3-dev python3-dev
-sudo python3 -m pip install six requests pyserial
+sudo python3 -m pip install -U six requests pyserial
 git clone --depth 1 https://github.com/airspy/airspyone_host.git /tmp/airspyone_host
 git clone --depth 1 https://github.com/greatscottgadgets/hackrf.git /tmp/hackrf
 git clone --depth 1 https://github.com/UCSD-E4E/radio_collar_tracker_comms /tmp/radio_collar_tracker_comms
@@ -21,19 +21,19 @@ make -j
 sudo make install
 sudo ldconfig
 
-sudo add-apt-repository ppa:ettusresearch/uhd
+sudo add-apt-repository -y ppa:ettusresearch/uhd
 sudo apt-get update
-sudo apt-get install libuhd-dev uhd-host
+sudo apt-get install -y libuhd-dev uhd-host
 sudo uhd_images_downloader -t b2xx*
 
 cd $(cwd)
 python3 -m venv .venv
 source .venv/bin/activate
 cd /tmp/radio_collar_tracker_comms
-python -m pip install .
+python -m pip install -U .
 cd $(cwd)
 git submodule update --init --recursive
-python -m pip install .
+python -m pip install -U .
 
 sudo echo "[Unit]" > /lib/systemd/system/rctrun.service
 sudo echo "Description=RCT OBC Runner" >> /lib/systemd/system/rctrun.service
